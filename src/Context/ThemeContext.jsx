1b4e149aa91b0f52e9b20fc1d5c201550376a7a2
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-07-14 14:04:22 +0530

    Restore ThemeContext in lowercase context folder

---
+++ src/Context/ThemeContext.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+//adding
+export function ThemeProvider({ children }) {
+    const [theme, setTheme] = useState(() => {
+        const savedTheme = localStorage.getItem("theme");
+
+        if (savedTheme) {
+            return savedTheme;
+        }
+        return window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+    });
+
+    useEffect(() => {
+        document.documentElement.classList.toggle("dark", theme === "dark");
+        localStorage.setItem("theme", theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+    };
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useTheme = () => useContext(ThemeContext);
