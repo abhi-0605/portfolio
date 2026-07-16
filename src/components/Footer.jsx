d664490fcf7b4d551811a00a4b5fe434e5cfc8a3
Abhinav Kumar <abhinavkumar0605.work@gmail.com>
2026-07-16 12:56:27 +0530

    improvement in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,9 +29,10 @@ function Footer() {
               href="https://github.com/abhi-0605"
               target="_blank"
               rel="noreferrer"
+              aria-label="GitHub Profile"
               className="flex items-center justify-center w-10 h-10 rounded-full bg-zinc-900 dark:bg-zinc-800 hover:bg-zinc-800 transition"
             >
-              <FaGithub className='text-[24px]'/>
+              <FaGithub className='text-[24px]' />
             </motion.a>
 
             <motion.a
@@ -39,40 +40,42 @@ function Footer() {
               href="https://www.linkedin.com/in/abhi0605/"
               target="_blank"
               rel="noreferrer"
+              aria-label="LinkedIn Profile"
               className="flex items-center justify-center w-10 h-10 rounded-full bg-zinc-900 dark:bg-zinc-800 hover:bg-blue-600 transition"
             >
-              <LiaLinkedin className='text-[28px]'/>
+              <LiaLinkedin className='text-[28px]' />
             </motion.a>
 
             <motion.a
               whileHover={{ scale: 1.2, y: -5 }}
               href="mailto:abhinavkumar18jun@gmail.com"
-              className='flex items-center justify-center w-10 h-10 rounded-full bg-zinc-900 dark:bg-zinc-800 hover:bg-red-500 transition'
+              aria-label="Send Email"
+              className="flex items-center justify-center w-10 h-10 rounded-full bg-zinc-900 dark:bg-zinc-800 hover:bg-red-500 transition"
             >
-              <MailIcon size={26}/>
+              <MailIcon size={26} />
             </motion.a>
 
           </div>
         </motion.div>
 
         <div className='my-8 h-px bg-linear-to-r from-transparent via-gray-600 dark:via-gray-700 to-transparent'></div>
-          <motion.div
-            initial={{ opacity: 0 }}
+        <motion.div
+          initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 0.2 }}
           viewport={{ once: true }}
           className="flex flex-col md:flex-row justify-between items-center gap-4 text-gray-400 dark:text-gray-500"
-          >
-            <p className='text-center md:text-left'
-            >© {new Date().getFullYear()} Abhinav Kumar. All Rights Reserved.</p>
+        >
+          <p className='text-center md:text-left'
+          >© {new Date().getFullYear()} Abhinav Kumar. All Rights Reserved.</p>
 
-            <p className='flex items-center justify-center gap-2 text-center'
-            >Made with
-              <Heart size={18} className='fill-red-500 text-red-500 animate-pulse'
-              />using React & Tailwind CSS
-            </p>
-          </motion.div>
-        
+          <p className='flex items-center justify-center gap-2 text-center'
+          >Made with
+            <Heart size={18} className='fill-red-500 text-red-500 animate-pulse'
+            />using React & Tailwind CSS
+          </p>
+        </motion.div>
+
       </div>
     </footer>
   )
